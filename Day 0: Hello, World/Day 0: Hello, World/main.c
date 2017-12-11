166d763612a9482cff65c3752d3fdee44cb8bb4e
igrik82 <igrik82@gmail.com>
2017-12-11 11:13:15 +0500

    Create project

---
+++ Day 0: Hello, World/Day 0: Hello, World/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Day 0: Hello, World
+//
+//  Created by Игорь on 11/12/2017.
+//  Copyright © 2017 Игорь. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
