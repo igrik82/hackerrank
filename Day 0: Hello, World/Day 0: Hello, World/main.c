0d6713311bf615e9a9c30c68e5737cdf17640e3c
igrik82 <igrik82@gmail.com>
2017-12-11 11:45:53 +0500

    Solved

--- Day 0: Hello, World/Day 0: Hello, World/main.c
+++ Day 0: Hello, World/Day 0: Hello, World/main.c
@@ -1,15 +1,17 @@
-//
-//  main.c
-//  Day 0: Hello, World
-//
-//  Created by Игорь on 11/12/2017.
-//  Copyright © 2017 Игорь. All rights reserved.
-//
-
 #include <stdio.h>
+#define LIMITS_OF_INPUT 105
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+    // Declare a variable named 'input_string' to hold our input.
+    char input_string[LIMITS_OF_INPUT];
+    
+    // Read a full line of input from stdin and save it to our variable, input_string.
+    fgets(input_string, LIMITS_OF_INPUT, stdin);
+    
+    // Print a string literal saying "Hello, World." to stdout using printf.
+    printf("Hello, World.\n%s",input_string);
+    
+    // TODO: Write a line of code here that prints the contents of input_string to stdout.
+    
     return 0;
 }
