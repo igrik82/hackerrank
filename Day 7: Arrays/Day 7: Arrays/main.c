bedb021695bcefed2b97c9a3fb381ec2e3e5793e
igrik82 <igrik82@gmail.com>
2017-12-19 15:09:14 +0500

    Solved

---
+++ Day 7: Arrays/Day 7: Arrays/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int *arr = NULL;
+    arr = malloc(sizeof(int) * n);
+    for(int arr_i = 0; arr_i < n; arr_i++){
+        scanf("%d",&arr[arr_i]);
+    }
+    
+    for (int i = n -1; i >= 0; --i){
+        printf("%d ", *(arr + i));
+    }
+    free(arr);
+}
